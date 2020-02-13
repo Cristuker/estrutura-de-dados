795b5128bfab002f423d501e7c1f0282d98b8359
Cristian Silva <cristian123105@gmail.com>
2020-02-13 13:31:03 -0300

    iniciando projeto de venda de ingressos

---
+++ VendaDeIngressos/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/*Requisitos
+-Controla número de pessoas, entrada e saída, lugares disponíveis;
+
+-Ao inicicializar, entrar com capacidade máxima;
+
+Menu:
+1-Comprar ingresso
+2-Devolver Ingresso
+3-Trocar de Lugar
+*/
+
+using namespace std;
+
+int main(){
+
+    int acao = 1;
+
+    while(true){
+        cout << "Menu" << cout;
+
+    }
+}

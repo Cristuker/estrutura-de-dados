f8dd7e1e14c7b8d468d7ba364f6207516e1b7daf
Cristian Silva <cristian123105@gmail.com>
2020-05-12 19:52:55 -0300

    add palindromo tp

--- pilhas/Palindromo/main.cpp
+++ pilhas/Palindromo/main.cpp
@@ -3,33 +3,68 @@
 
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
+    int opcao;
+
     Pilha<char> pilha(50);
-
-    cout << "Palindromo" << endl;
     string frase;
-    cout <<"Digite a frase?" << endl;
-    cin >> frase;
-
-    for(int i = 0; i < frase.length(); i++){ //frase.length() pode ser frase.size()
-       pilha.empilha(frase[i]); // frase.at(i) Por colocar na pilha a frase será invertida 
-    }
 
 
-    for(int i = 0; i < frase.size(); i++){ 
-        if(frase.at(i) == pilha.elementodotopo()){
-            pilha.desempilha();
-        }else{
+
+    do {
+        cout << "----Palindromo-----" << endl;
+        cout << "1 - Testar uma nova frase" << endl;
+        cout << "0 - Sair" << endl;
+        cout << "-> ";
+        cin >> opcao;
+
+        switch (opcao) {
+        case 1:
+
+            if(!pilha.pilhavazia()){
+                for(int index = 0; index >= pilha.topo; index++ ){
+                    pilha.desempilha();
+                }
+            }
+
+            cout << "Digite uma frase: " << endl;
+            cout << "-> ";
+            cin >> frase;
+            for (int i = 0; i < frase.length(); i++) { //frase.length() pode ser frase.size()
+                if (!(frase[i] == ' ')) {
+                    pilha.empilha(frase[i]); // frase.at(i) Por colocar na pilha a frase será invertida
+                }
+            }
+
+            for (int i = 0; i < frase.size(); i++) {
+                if (frase.at(i) == pilha.elementodotopo()) {
+                    pilha.desempilha();
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (pilha.pilhavazia()){
+                cout << "É um palindromo" << endl;
+            }
+            else {
+                cout << "Não é um palindromo" << endl;
+            }
+            
+            pilha.clearDisplay();
+
+            break;
+        case 0: 
+            exit(0);
+            break;
+        default:
+            cout << "Digite uma opção válida" << endl;
+            pilha.clearDisplay();
             break;
         }
-    }
+    } while (opcao != 0);
 
-    if(pilha.pilhavazia()){
-        cout << "É um palindromo" << endl;
-    }else{
-        cout << "Não é um palindromo" << endl;
-    }
-    
-    return 0;
 }
